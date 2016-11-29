0c57d6426e81e9eca56bd6693485d1da1d285114
arnondora <peter_arnon1@hotmail.com>
2016-11-29 15:41:29 +0700

    Add Footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="navbar navbar-default navbar-fixed-bottom">
+   <div class="container">
+     <p class="navbar-text pull-left">News Search Engine</p>
+
+     <p class= "navbar-text pull-right"><span class = "fa fa-code"></span> with <span style = "color:#E91E63" class = "fa fa-heart"></span> by @arnondora</p>
+   </div>
+</div>

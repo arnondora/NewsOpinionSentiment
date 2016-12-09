dc33f9ce2d295dc057b5968a5c01bae0ed17ddde
arnondora <peter_arnon1@hotmail.com>
2016-12-09 22:14:01 +0700

    Add newNewsPublisher() to add new news publisher

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\NewsPublisher;
+
 class NewsController extends Controller
 {
     public function showFeatureNews ()
     {
       return (String) view('news.feature');
+    }
+
+    public function newNewsPublisher (Request $request)
+    {
+      $publisher = new NewsPublisher();
+      $publisher->name = $request->name;
+      $publisher->url = $request->url;
+
+      $publisher->save();
+      return back()->with('status','Add News Publisher Successfully');
     }
 }

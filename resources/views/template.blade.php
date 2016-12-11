6a1025432f0cfef1735229090fdb2e0bd13bf1bd
arnondora <peter_arnon1@hotmail.com>
2016-12-11 18:08:30 +0700

    Import jqcloud js and css into web template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/jqcloud.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
         <title>@yield('title') - News Search Engine</title>
 
@@ -29,6 +30,7 @@
     </body>
 
     <script src = "/js/jquery.min.js"></script>
+    <script src = '/js/jqcloud.min.js'></script>
     <script src="https://use.fontawesome.com/05d65594f4.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
